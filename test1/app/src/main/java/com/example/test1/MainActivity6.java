486e1fe297b909623c25c05f84ed8153c32f010c
userjp7 <38785002+userjp7@users.noreply.github.com>
2022-03-28 07:54:44 +0530

    main menu tex

--- test1/app/src/main/java/com/example/test1/MainActivity6.java
+++ test1/app/src/main/java/com/example/test1/MainActivity6.java
@@ -3,21 +3,12 @@ package com.example.test1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity6 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
-        Bundle bundle = getIntent().getExtras();
-        if (bundle  != null) {
-            String name = bundle.getString("name");
-            String age = bundle.getString("age");
-            TextView tvName = findViewById(R.id.tvName);
-            TextView tvAge = findViewById(R.id.tvAge);
-            tvName.setText(name);
-            tvAge.setText(age);
-        }
     }
 }
